6bd0e903029dfbc61e9a42ca16e4dae7cc53fca3
Azat Hairullin <azat.hairullin@gmail.com>
2021-02-07 00:36:23 +0300

    Add Employee entity

---
+++ src/main/java/tech/manager/entity/Employee.java
@@ -0,0 +1,40 @@
+package tech.manager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Table(name = "employee")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "job_title")
+    private String jobTitle;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "employee_code", nullable = false)
+    private String employeeCode;
+}
